77c0ce8994c6f8f6b9b2290dad5c2f1ae2ce09b7
Arpitpundir <thakurarpitpundir73@gmail.com>
2023-03-05 10:43:57 +0530

    Solved K-closest points to origin

--- k-Closest-Points-To-Origin.cpp
+++ k-Closest-Points-To-Origin.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-    bool compare(vector<int> &a,vector<int> &b){
-        return (a[0]*a[0]+a[1]*a[1])<(b[0]*b[0]+b[1]*b[1]);
-    }
-    int quickSelect(vector<vector<int> > &a,int &l,int &r){
-        if(l==r)
-            return l;
-        int i=l+1;
-        for(int j=l+1;j<=r;j++){
-            if(compare(a[j],a[l])){
-                swap(a[i++],a[j]);
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>> maxHeap;
+        for (auto& p : points) {
+            int x = p[0], y = p[1];
+            maxHeap.push({x*x + y*y, x, y});
+            if (maxHeap.size() > k) {
+                maxHeap.pop();
             }
         }
-        swap(a[i-1],a[l]);
-        return i-1;
-    }
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        int l=0,r=points.size()-1;
-        while(l<=r){
-            int mid=quickSelect(points,l,r);
-           // cout<<l<<" "<<mid<<" "<<r<<endl;
-            if(mid==K){
-                break;
-            }else if(mid<K){
-                l=mid+1;
-            }else{
-                r=mid-1;
-            }
-        }
-        vector<vector<int> > ans;
-        for(int i=0;i<K;i++){
-            ans.push_back(points[i]);
+        vector<vector<int>> ans(k);
+        for (int i = 0; i < k; ++i) {
+            vector<int> top = maxHeap.top();
+            maxHeap.pop();
+            ans[i] = {top[1], top[2]};
         }
         return ans;
     }
